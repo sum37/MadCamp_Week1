848bc3c56b302400307b0528366d0fa2cd4b6c8c
st20200468 <sum37@kaist.ac.kr>
2024-06-29 22:43:35 +0900

    before merge

--- app/src/main/java/com/example/week1/ui/Phone/PhoneFragment.kt
+++ app/src/main/java/com/example/week1/ui/Phone/PhoneFragment.kt
@@ -9,33 +9,32 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.week1.databinding.FragmentPhoneBinding
 
-
 class PhoneFragment : Fragment() {
 
-private var _binding: FragmentPhoneBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val PhoneViewModel =
+    private var _binding: FragmentPhoneBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    // FragmentPhoneBinding이 본 페이지에 해당하는 xml 과 이 파일을 연결해주는거임
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, // xml layout file을 view 객체로 보여준다
+        container: ViewGroup?, //
+        savedInstanceState: Bundle? // 이전에 저장한 상태 데이터를 사용해 프래그먼트 복제, 화면회전과 같은 상황에서 fragment유지할 수 있음
+    ): View {
+
+        val view =
             ViewModelProvider(this).get(PhoneViewModel::class.java)
+        // viewModelProvider는 ViewModel을 생성하고 반환한다.
+        _binding = FragmentPhoneBinding.inflate(inflater, container, false)
+        // 여기서 fragment_phone을 view로 보여주는거임
+        val root: View = binding.root
+        // binding.root는 여기서 생성된 FragmentPhoneBinding의 최상위 View를 참조하는겁니다.
 
-    _binding = FragmentPhoneBinding.inflate(inflater, container, false)
-    val root: View = binding.root
-
-    val textView: TextView = binding.textPhone
-    PhoneViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+        return root // binding의 root view를 return 한다.
     }
-    return root
-  }
 
-override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
